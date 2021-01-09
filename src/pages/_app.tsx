79090f8c0c4bf07a603a7518e84256e5374e4610
Gabriel <gabrielmendoncapereira14@gmail.com>
2021-01-09 16:15:00 -0300

    Adding pages _app and _document to project next

---
+++ src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Post Machine - Boilerplate</title>
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+      <h1>Post Machine - Boilerplate</h1>
+    </div>
+  )
+}
+
+export default Home;
